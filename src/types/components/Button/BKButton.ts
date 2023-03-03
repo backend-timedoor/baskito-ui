77f8795ac330362deaba8bb6912bff6b27f5488c
rizkyhidayattulloh <rizkynear1@gmail.com>
2023-03-03 11:26:01 +0800

    initial commit

---
+++ src/types/components/Button/BKButton.ts
@@ -0,0 +1,21 @@
+export type Variant =
+  | "primary"
+  | "secondary"
+  | "danger"
+  | "success"
+  | "warning"
+  | "info"
+  | "light"
+  | "dark"
+  | "outline-primary"
+  | "outline-secondary"
+  | "outline-danger"
+  | "outline-success"
+  | "outline-warning"
+  | "outline-info"
+  | "outline-light"
+  | "outline-dark";
+
+export type Size = "sm" | "md" | "lg";
+
+export type Shape = "default" | "pill" | "square";
---
+++ src/types/components/index.ts
@@ -0,0 +1,2 @@
+// BKButton
+export type { Shape as BKButtonShape, Size as BKButtonSize, Variant as BKButtonVariant } from './Button/BKButton';
