d10fa9ab90d013a7a15f536c63a13c413382c1a9
rizkyhidayattulloh <rizkynear1@gmail.com>
2023-03-08 09:50:32 +0800

    add BKPaginationComponent

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export {default as BKButton} from './Button/BKButton.vue';
-export {default as BKLinkButton} from './Button/BKLinkButton.vue';
+export { default as BKButton } from "./Button/BKButton.vue";
+export { default as BKLinkButton } from "./Button/BKLinkButton.vue";
+export { default as BKPagination } from "./Pagination/BKPagination.vue";
--- src/types/components/index.ts
+++ src/types/components/index.ts
@@ -1,2 +1,14 @@
 // BKButton
-export type { Shape as BKButtonShape, Size as BKButtonSize, Variant as BKButtonVariant } from './Button/BKButton';
+export type {
+  Shape as BKButtonShape,
+  Size as BKButtonSize,
+  Variant as BKButtonVariant,
+} from "./Button/BKButton";
+
+// BKPagination
+export type {
+  PaginationLink as BKPaginationLink,
+  PaginationMeta as BKPaginationMeta,
+  ResourceCollection as BKResourceCollection,
+  PaginationData as BKPaginationData,
+} from "./Pagination/BKPagination";
